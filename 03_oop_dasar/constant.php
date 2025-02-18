c8945d1ad45dfc64925d7185796a396f33a60948
ak sigma aka Nathan <livyathan142008@gmail.com>
2025-02-18 11:51:14 +0700

    constant

---
+++ 03_oop_dasar/constant.php
@@ -0,0 +1,45 @@
+<?php
+
+// define('SAYURAN','Sawi');
+// echo SAYURAN;
+
+// echo "<br>";
+
+// const BANYAK = 21;
+// echo BANYAK;
+
+
+
+
+
+
+// class iya {
+//    const KABEL = 'hdmi';
+// }
+
+// echo iya::KABEL;
+
+//magic constant
+// echo __FILE__;
+// echo __LINE__;
+// echo __DIR__;
+// echo __CLASS__;
+// echo __FUNCTION__;
+// echo __METHOD__;
+// echo __TRAIT__;
+// echo __NAMESPACE__;
+
+
+// function cobak() {
+//     return __FUNCTION__;
+// }
+
+// echo cobak();
+
+
+// class cobak{
+//     public $kelaz = __CLASS__;
+// }
+
+// $obj = new cobak;
+// echo $obj->kelaz;
